a1df4c04d987f4505e534b456155837112c4e64d
Eric S. Tellez <donsadit@gmail.com>
2017-05-28 12:50:35 -0500

    add script arguments and docstrings

--- neardup-simsearch.jl
+++ neardup-simsearch.jl
@@ -2,8 +2,6 @@ using SimilaritySearch
 using TextModel
 using JSON
 
-const NUM_FAST_LINKS = 7
-
 function set_oracle(index, fastlinks::Dict{UInt64,KnnResult})
     function oracle(q::VBOW)::Vector{Int32}
         L = Int32[]
@@ -22,10 +20,10 @@ function set_oracle(index, fastlinks::Dict{UInt64,KnnResult})
     fastlinks
 end
 
-function update_oracle(index, fastlinks, bow::VBOW)
+function update_oracle(index, fastlinks, bow::VBOW, num_fast_links)
     for term in bow.tokens
         if !haskey(fastlinks, term.id)
-            fastlinks[term.id] = KnnResult(NUM_FAST_LINKS)
+            fastlinks[term.id] = KnnResult(num_fast_links)
         end
         push!(fastlinks[term.id], length(index.db), term.weight)
     end
@@ -37,14 +35,14 @@ function create_index()
     config.qlist = [5]
     #config.qlist = [2, 3, 5]
     config.skiplist = []
-    
+
     index = LocalSearchIndex(VBOW, angle_distance, recall=0.90, neighborhood=Nullable{NeighborhoodAlgorithm}(LogSatNeighborhood(1.5)))
     fastlinks = set_oracle(index, Dict{UInt64,KnnResult}())
     # index.options.verbose = false
     return index, config, fastlinks
 end
 
-function main(filename)
+function main(filename, dup_threshold, key, num_fast_links)
     index, config, fastlinks = create_index()
     lineno = 0
     DB = []
@@ -52,14 +50,14 @@ function main(filename)
     iterlines(filename) do line
         tweet = TextModel.parsetweet(line)
         lineno += 1
-        bow = compute_bow(tweet["text"], config) |> VBOW
+        bow = compute_bow(tweet[key], config) |> VBOW
 
         knn, N = find_neighborhood(index, bow)
-        if length(knn) > 0 && first(knn).dist < 1.0
-            info("VERY NEAR OBJECT:", first(knn), line, "; ORIGINAL:", DB[first(knn).objID])
+        if length(knn) > 0 && first(knn).dist < dup_threshold
+            info("NEAR OBJECT:", first(knn), line, "; ORIGINAL:", DB[first(knn).objID])
         else
             push_neighborhood!(index, bow, N, length(index.db))
-            update_oracle(index, fastlinks, bow)  # adding fast links
+            update_oracle(index, fastlinks, bow, num_fast_links)  # adding fast links
             push!(DB, line)
             println(line)
         end
@@ -67,8 +65,37 @@ function main(filename)
         if lineno % 1000 == 0
             info("selected $(length(DB)) of $(lineno)")
         end
-        
+
     end
 end
 
-main(ARGS[1])
+if length(ARGS) == 0
+    info("""
+    neardup-simsearch.jl removes the number of near duplicated items in text databases. It keeps only the original version of the document.
+
+    Usage: [environment-variables] julia neardup-simsearch.jl files...
+
+    Each input file must contain a json-dictionary per line, the indexes are independent per file
+
+    For simplicity, the arguments are passed as environment-variables
+
+    - dup_threshold: items under this value are considered as duplicated
+       valid range: 0 < dup_threshold < pi/2
+       default: 1.0
+
+    - key: the keyword containing the text for each json
+       default: text
+
+    - num_fast_links: the number of documents stored per token to help localsearch indexes to perform fast lookups
+       default: 7
+    """)
+else
+    for filename in ARGS
+        main(
+            filename,
+            parse(Float64, get(ENV, "dup_threshold", "1.0")),
+            get(ENV, "key", "text"),
+            parse(Int, get(ENV, "num_fast_links", "7"))
+        )
+    end
+end
